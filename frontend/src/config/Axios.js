ea8b2ff139744d5b243f6ffa09ff11075dd9c215
carmdp <claudio@cr.net.ar>
2022-09-27 17:10:34 -0300

    last update

--- frontend/src/config/Axios.js
+++ frontend/src/config/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const proto = 'http'; //Protocol web hhtp or https
-const host =  '192.168.1.10'; //url or ip or localhost
+const host =  'localhost'; //url or ip or localhost
 const port = '3005'; // ports conection api
 
 const conection = axios.create({baseURL:`${proto}://${host}:${port}`});
